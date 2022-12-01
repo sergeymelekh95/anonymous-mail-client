3b8b65d7d6982b3ac07e0fcb12b5e23ea57181ea
sergeymelekh95 <sergeymelekh95@mail.ru>
2022-12-02 03:50:08 +0400

    fix baske url

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 // export const BASE_URL = 'http://localhost:3001';
-export const BASE_URL = 'https://anonymous-mail.onrender.com';
+export const BASE_URL = 'https://anonymous-mail-api.onrender.com';
